5a3369e35faa73d6410eff3f2fb92d6cbebe9fe7
Rishi Sharma <39599919+kampaitees@users.noreply.github.com>
2019-03-13 16:48:48 +0530

    Create Variable BestFit.c

---
+++ Variable BestFit.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#define max 25
+void main() {
+
+	int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0  ;
+	static int bf[max],ff[max];
+	
+	printf("\nMemory Management Scheme - Worst Fit");
+	
+	printf("\nEnter the number of blocks:");
+	scanf("%d",&nb);
+	
+	printf("Enter the number of processes:");
+	scanf("%d",&nf);
+	
+	printf("\nEnter the size of the blocks:-\n");
+	
+	for(i=1;i<=nb;i++) {
+		printf("Block %d:",i);
+		scanf("%d",&b[i]);
+	}
+	
+	printf("Enter the size of the processes :-\n");
+	
+	for(i=1;i<=nf;i++) {
+		printf("Process %d:",i);
+		scanf("%d",&f[i]);
+	}
+	for(i=1;i<=nf;i++) {
+		for(j=1;j<=nb;j++) {
+			if(bf[j]!=1) { //if bf[j] is not allocated 
+				temp=b[j]-f[i];
+				if(temp>=0)
+					if(highest<temp) {
+						ff[i]=j;
+						highest=temp;
+					}
+			}
+		}
+		
+		frag[i]=highest;
+		bf[ff[i]]=1;
+		highest=0;
+	}
+	
+	printf("\nProcess_no:\tProcess_size :\tBlock_no:\tBlock_size:\tFragement");
+	
+	for(i=1;i<=nf;i++) {
+		
+		if(frag[i] == 0) {
+			
+			printf("\n%d\t\t%d\t\t", i, f[i]);
+			printf("Not Allocated\t-----\t\t-----");
+		}
+		
+		else
+			printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
+	}
+	int count = 0;
+	printf("\n");
+	for(i=0;i<=nb;i++) {
+		if(bf[i] != 1) 
+			count += b[i];
+	}
+	printf("External fragment: %d", count);
+	printf("\n");
+}
